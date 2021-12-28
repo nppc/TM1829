a86b7be43e459748022d64fc87896d28e77779b2
Palonen <ppalonen@bethel.jw.org>
2021-12-28 15:51:15 +0100

    pwm added

--- src/tm1829.h
+++ src/tm1829.h
@@ -3,5 +3,6 @@
 #define SRC_TM1829_H_
 
 extern void sendCurrentRGB(uint8_t r, g, b);
-extern void oneWire_sendByte(uint8_t bt);
+void sendPwmRGB(uint8_t r, g, b);
+//extern void oneWire_sendByte(uint8_t bt);
 #endif /* SRC_TM1829_H_ */
